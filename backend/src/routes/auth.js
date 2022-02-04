1cd67f00f8e5a774a1c5fd20fd51a4bf4ba6fd05
이윤학 <dbsgkr0203@gmail.com>
2022-02-04 20:11:13 +0900

    feat: auth middleware 로그인 체크 추가

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,6 @@
 const authCtrl = require('../controllers/auth');
 const router = require('express').Router();
-
+const { checkLogin } = require('../middlewares/auth');
 router.get('/login/success', authCtrl.loginSuccess);
 router.get('/login/failed', authCtrl.loginFailed);
 router.get('/logout', authCtrl.logout);
@@ -17,13 +17,13 @@ router.post('/signin', authCtrl.signIn);
 router.get('/signout', authCtrl.signOut);
 
 // 회원 정보 등록
-router.post('/:id/profile', authCtrl.update);
+router.post('/:id/profile', checkLogin, authCtrl.update);
 // 회원 정보 조회
 router.get('/:id/profile', authCtrl.read);
 // 비밀번호 찾기
 router.post('/find-password', authCtrl.findPassword);
 
 // 비밀번호 변경
-router.patch('/:id/profile/password', authCtrl.changePassword);
+router.patch('/:id/profile/password', checkLogin, authCtrl.changePassword);
 
 module.exports = router;
