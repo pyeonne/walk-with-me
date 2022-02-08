6e294c22c50d74b976e44fca2296d3148fbe6548
yonggyu <kyg96917@gmail.com>
2022-02-08 16:46:40 +0900

    feat: Map 컴포넌트 추가 (#37)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Map from './components/Map/Map.jsx';
 
 function App() {
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
-  );
+  return <Map area='매탄3동' />;
 }
 
 export default App;
