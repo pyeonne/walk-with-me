9f08d5c6043ed9b220db50785f7c1fa47d115f0b
yonggyu <kyg96917@gmail.com>
2022-02-09 17:53:16 +0900

    feat: logo에 홈으로 가는 기능 추가

--- frontend/src/components/Header/Logo.jsx
+++ frontend/src/components/Header/Logo.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import logoHeader from './images/logoHeader.svg';
 import logoSignPage from './images/logoSignPage.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Logo = (props) => {
-  return <img src={props.type == 'col' ? logoSignPage : logoHeader} />;
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    navigate('/');
+  };
+
+  return (
+    <img
+      src={props.type == 'col' ? logoSignPage : logoHeader}
+      onClick={onClickHandler}
+    />
+  );
 };
 
 export default Logo;
