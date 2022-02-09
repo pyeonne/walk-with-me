73a91c6c734da559ce6c294a7337dafc3b611e77
yonggyu <kyg96917@gmail.com>
2022-02-09 17:44:57 +0900

    feat: 비밀번호찾기 페이지 추가 (#46)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import SignUp from './pages/auth/SignUp';
 import Home from './pages/Home';
 import SignIn from './pages/auth/SignIn';
+import PasswordFind from './pages/auth/PasswordFind';
 import ProfileRegister from './pages/profile/ProfileRegister';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/signup' element={<SignUp />} />
       <Route path='/signin' element={<SignIn />} />
+      <Route path='/password-find' element={<PasswordFind />} />
       <Route path='/profile-register' element={<ProfileRegister />} />
     </Routes>
   );
