092c656308f1b6593bcb071e21b270cf505a45b3
dinomoon <ansrud45@gmail.com>
2022-02-11 19:37:59 +0900

    refactor: header 리팩토링 navigate -> Link

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Header.module.css';
 import darkMode from './images/darkMode.svg';
 import Avatar from '../Avatar/Avatar';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from './Logo.jsx';
+import { Context } from '../../context';
+import { CHANGE_USER_INFO } from '../../context/actionTypes';
 
 const Header = (props) => {
-  const navigate = useNavigate();
+  const [state, dispatch] = useContext(Context);
+  const user = state.user;
+
+  const clickHandler = () => {
+    dispatch({ type: CHANGE_USER_INFO, payload: null });
+    fetch('http://localhost:4000/api/auth/signout');
+  };
 
   return (
     <header className={styles['nav-bar']}>
       <div className={styles.wrapper}>
         <Logo className={styles.logo} type='row' />
         <div className={styles.right}>
-          {props.isLoggedIn ? (
+          {user ? (
             <Avatar src={props.src} width='4rem' height='4rem' />
           ) : (
-            <button
-              className={styles['sign-in']}
-              onClick={() => navigate('/signin')}
-            >
-              로그인
+            <button className={styles['sign-in']}>
+              <Link to='/signin'>로그인</Link>
             </button>
           )}
-          <button
-            className={styles['sign-up']}
-            onClick={
-              props.isLoggedIn
-                ? () => {
-                    navigate('/');
-                  }
-                : () => {
-                    navigate('/signup');
-                  }
-            }
-          >
-            {props.isLoggedIn ? '로그아웃' : '회원가입'}
-          </button>
+
+          {user ? (
+            <button className={styles['logout']} onClick={clickHandler}>
+              로그아웃
+            </button>
+          ) : (
+            <button className={styles['sign-up']}>
+              <Link to='/signup'>회원가입</Link>
+            </button>
+          )}
           <img className={styles['dark-mode']} src={darkMode} />
         </div>
       </div>
