fbf55c59cfe8e069478e6b46ecbc654afd7a1882
이윤학 <dbsgkr0203@gmail.com>
2022-02-12 00:32:03 +0900

    etc: 참가신청 모달 레이아웃 작업

---
+++ frontend/src/Sss.jsx
@@ -0,0 +1,11 @@
+import RequestModal from './components/Modal/RequestModal';
+const sss = () => {
+  return (
+    <div>
+      gd
+      <RequestModal isOpen={true} />
+    </div>
+  );
+};
+
+export default sss;
--- frontend/src/components/Modal/RequestModal.jsx
+++ frontend/src/components/Modal/RequestModal.jsx
@@ -1,6 +1,7 @@
 import Modal from 'react-modal';
 import Button from '../Button/Button';
 import styles from './RequestModal.module.css';
+import Input from '../Input/Input';
 const RequestModal = (props) => {
   return (
     <Modal
@@ -8,8 +9,8 @@ const RequestModal = (props) => {
       ariaHideApp={false}
       style={{
         content: {
-          height: '50%',
-          width: '40%',
+          height: '40rem',
+          width: '50rem',
           left: '50%',
           top: '50%',
           padding: '3rem',
@@ -17,20 +18,30 @@ const RequestModal = (props) => {
         },
       }}
     >
-      <div className={styles.content}></div>
-      <div className={styles['btn__group']}>
-        <Button
-          text='닫기'
-          height='5rem'
-          width='20rem'
-          onClick={!props.isOpen}
-        />
-        <Button
-          text='전송'
-          height='5rem'
-          width='20rem'
-          onClick={!props.isOpen}
-        />
+      <div className={styles['modal__container']}>
+        <div className={styles['modal__header']}>
+          <h2>자기소개를 입력해주세요!</h2>
+        </div>
+        <form method='POST'>
+          <div className={styles['form__group']}>
+            <Input name='content' height='20rem' />
+            <div className={styles['btn__group']}>
+              <Button
+                type='button'
+                text='닫기'
+                height='5rem'
+                width='18.2rem'
+                // onClick={!props.isOpen}
+              />
+              <Button
+                text='전송'
+                height='5rem'
+                width='18.2rem'
+                // onClick={!props.isOpen}
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </Modal>
   );
