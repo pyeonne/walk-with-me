9e3abd9d1b5e0f6e51f09e46d057168e4d7de91b
yonggyu <kyg96917@gmail.com>
2022-02-15 23:46:53 +0900

    feat: 회원가입, 로그인 페이지 다크모드 완성

--- frontend/src/components/Input/Input.jsx
+++ frontend/src/components/Input/Input.jsx
@@ -35,7 +35,7 @@ const StyledInput = styled.input`
   width: ${(props) => props.width || '50rem'};
   height: ${(props) => props.height || '6rem'};
   padding-left: 6rem;
-  background-color: var(--body-background-color);
+  background-color: var(--input-background-color);
 `;
 
 const StyledTextArea = styled.textarea`
@@ -43,7 +43,7 @@ const StyledTextArea = styled.textarea`
   height: ${(props) => props.height || '26rem'};
   padding: 1rem;
   line-height: 1.4;
-  background-color: var(--body-background-color);
+  background-color: var(--input-background-color);
 `;
 
 const Input = forwardRef((props, ref) => {
