a3af167a166bf325e964a6703cd55f5065dd62c9
이윤학 <dbsgkr0203@gmail.com>
2022-02-17 15:42:42 +0900

    etc: 안쓰는 스키마 제거

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = require('./schemas/user');
 const PostSchema = require('./schemas/post');
-const RoomSchema = require('./schemas/room');
-const ChatSchema = require('./schemas/chat');
 
 module.exports = {
   User: mongoose.model('User', UserSchema),
   Post: mongoose.model('Post', PostSchema),
-  Room: mongoose.model('Room', RoomSchema),
-  Chat: mongoose.model('Chat', ChatSchema),
 };
